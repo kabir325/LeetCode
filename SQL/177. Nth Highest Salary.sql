e968909c62a4a5f48acb68ea81015b4bee850421
Kabir <119738554+kabir325@users.noreply.github.com>
2025-08-03 16:36:04 +0530

    Create 177. Nth Highest Salary.sql

---
+++ SQL/177. Nth Highest Salary.sql
@@ -0,0 +1,15 @@
+#MySQL code
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE offset_val INT;
+  SET offset_val = N - 1;
+
+  RETURN (
+    SELECT salary FROM (
+      SELECT DISTINCT salary
+      FROM Employee
+      ORDER BY salary DESC
+      LIMIT offset_val, 1
+    ) AS Temp
+  );
+END
