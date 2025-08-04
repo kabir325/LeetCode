ca894cf91beb209ee540bbc410e4cb3ae2ee8cb4
Kabir <119738554+kabir325@users.noreply.github.com>
2025-08-04 09:02:55 +0530

    Create 262. Trips and Users.sql

---
+++ SQL/262. Trips and Users.sql
@@ -0,0 +1,14 @@
+# MySQL code
+# Write your MySQL query statement below
+SELECT 
+    t.request_at AS Day,
+    ROUND(
+        SUM(CASE WHEN t.status IN ('cancelled_by_driver', 'cancelled_by_client') THEN 1 ELSE 0 END) 
+        / COUNT(*), 
+        2
+    ) AS `Cancellation Rate`
+FROM Trips t
+JOIN Users c ON t.client_id = c.users_id AND c.banned = 'No' AND c.role = 'client'
+JOIN Users d ON t.driver_id = d.users_id AND d.banned = 'No' AND d.role = 'driver'
+WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at;
